3c6a2b4938c4249a43b0b4e71981276158441a1e
Milko Samuel Roine <mroine@lxplus947.cern.ch>
2026-06-22 09:33:29 +0200

    shell script for easier ex and to force LGG as source

--- workflow/run.sh
+++ workflow/run.sh
@@ -1,2 +1,2 @@
 source /cvmfs/sft.cern.ch/lcg/views/LCG_109/x86_64-el9-gcc15-opt/setup.sh
-exec python3 -m snakemake --latency-wait 120 "$@"
+exec snakemake --latency-wait 120 --cores="all" --config "$@"
